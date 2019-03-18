4e901d975ac7f5714af4aeeb90b45513dfb4e83f
Irene Ma <irene.ma@outlook.com>
2019-03-18 23:35:21 +1100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -34,6 +34,7 @@ var cards = [
 var cardsInPlay = [];
 
 var checkForMatch = function() {
+
 	if (cardsInPlay[0] === cardsInPlay[1]) {
   		console.log("You found a match!");
 	} else {
@@ -41,16 +42,31 @@ var checkForMatch = function() {
 	}
 };
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
+
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 
 	cardsInPlay.push(cards[cardId].rank);
+
+	this.setAttribute('src', cards[cardId].cardImage);
+
 	checkForMatch();
 };
 
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+    	var cardElement = document.createElement('img');
 
+    	cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
 
-flipCard(0);
-flipCard(2);
+		document.getElementById('game-board').appendChild(cardElement);
+
+	}
+}
+
+createBoard();
